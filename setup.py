de04c3ec04e9301d4d3a6817e18a6b0c7bc258f0
hmtmcse <hmtm.cse@gmail.com>
2022-01-04 16:25:23 +0600

    Project Setup and Essential Things are added (bismillah sw)

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='PF-Flask-Rest-Com',
+    version='1.0.0',
+    url='https://github.com/problemfighter/pf-flask-rest-com',
+    license='Apache 2.0',
+    author='Problem Fighter',
+    author_email='problemfighter.com@gmail.com',
+    description='Flask REST API Common Implementation by Problem Fighter Library',
+    long_description=__doc__,
+    packages=find_packages(),
+    zip_safe=False,
+    include_package_data=True,
+    platforms='any',
+    install_requires=[],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
