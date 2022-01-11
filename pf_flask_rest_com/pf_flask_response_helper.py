450e0831897c003808f0fcd753ee74e80a31b441
hmtmcse <hmtm.cse@gmail.com>
2022-01-11 21:17:29 +0600

    preparing response system

--- pf_flask_rest_com/pf_flask_response_helper.py
+++ pf_flask_rest_com/pf_flask_response_helper.py
@@ -1,4 +1,31 @@
+import json
+from flask import make_response
+from pf_flask_rest_com.common.pf_flask_rest_com_const import RestComConst
 
 
 class ResponseHelper:
-    pass
+
+    def response(self, response_data, code=200, headers: dict = None):
+        response_obj = make_response(response_data, code)
+        if headers:
+            response_obj.headers.update(headers)
+        return response_obj
+
+    def json_string_response(self, json_string: str, code=200, headers: dict = None):
+        _headers = {
+            RestComConst.CONTENT_TYPE: RestComConst.APPLICATION_JSON
+        }
+        if headers and isinstance(headers, dict):
+            _headers.update(headers)
+        return self.response(json_string, code=code, headers=_headers)
+
+    def json_response(self, obj, code=200, headers: dict = None):
+        json_object = {}
+        if obj:
+            json_object = obj
+        response_string = "{}"
+        try:
+            response_string = json.dumps(json_object)
+        except Exception as e:
+            response_string = "{}"
+        return self.json_string_response(response_string, code, headers)
