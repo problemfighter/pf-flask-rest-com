a6c884cf7bef9918746f6a5339494799613ec5ed
hmtmcse <hmtm.cse@gmail.com>
2022-02-15 10:53:07 +0600

    Adding features

--- pf_flask_rest_com/api_def.py
+++ pf_flask_rest_com/api_def.py
@@ -7,4 +7,13 @@ class APIDef(Schema):
 
 
 class FileField(fields.String):
-    pass
+    max_size_kb: int = None
+    allowed_extensions: list = None
+
+    def set_max_size_kb(self, size: int):
+        self.max_size_kb = size
+        return self
+
+    def set_allowed_extension(self, extension: list):
+        self.allowed_extensions = extension
+        return self
